c02f66e5f42ae86e21338733adc7472df906a0bd
rxshabN <nagwanirishab@gmail.com>
2025-01-04 19:57:42 +0530

    feature implemented

--- src/features/tasks/components/create-task-form.tsx
+++ src/features/tasks/components/create-task-form.tsx
@@ -211,6 +211,9 @@ export const CreateTaskForm = ({
                             </div>
                           </SelectItem>
                         ))}
+                        <SelectItem value="">
+                          <div>None</div>
+                        </SelectItem>
                       </SelectContent>
                     </Select>
                   </FormItem>
